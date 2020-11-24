b7584cab85ad3716f2fb34b4023ef9812a52c809
Samuel Meyers <srmeyers@gmail.com>
2020-11-24 15:28:45 -0600

    Initialize editor with page /json

--- background.js
+++ background.js
@@ -1,6 +1,23 @@
 chrome.browserAction.onClicked.addListener((tab) => {
+  let json1Param = ''
+  if (tab && tab.url) {
+    const tabURL = new URL(tab.url)
+    if (tabURL) {
+      const protocol = tabURL.protocol
+      const host = tabURL.host
+      const pathArray = tabURL.pathname.split('/')
+      let path = ''
+      if (pathArray.length > 1) {
+        path = pathArray[1]
+      }
+      if (protocol && host && path) {
+        json1Param = `?json1=${protocol}//${host}/${path}/json`
+      }
+    }
+  }
+
   chrome.tabs.create({
-    'url': chrome.extension.getURL('src/index.html'),
+    'url': chrome.extension.getURL(`src/index.html${json1Param}`),
     'active': true
   })
 })
--- src/main.js
+++ src/main.js
@@ -1,84 +1,108 @@
-var editor1 = monaco.editor.create(document.getElementById('editor1'), {
-  value: [
-    '{ "stuff": { "that": [1,2,3], "isin": true, "json": "end"}}',
-  ].join('\n'),
-  language: 'json',
-  tabSize: 2,
-  formatOnPaste: true,
-  minimap: { enabled: false }
-});
+let params = (new URL(document.location)).searchParams;
+let json1Url = params.get('json1');
 
-document.getElementById("format-edit-1").addEventListener("click", formatJson1)
-function formatJson1() {
-  editor1.getAction("editor.action.formatDocument").run()
+if (json1Url) {
+  fetch(json1Url)
+  .then(response => response.json())
+  .then(json => buildEditor(json))
+  .catch(error => {
+    window.console.log('error: ', error)
+    buildEditor() // show editor with default values
+  })
+} else {
+  buildEditor()
 }
 
-document.getElementById("paste-1").addEventListener("click", paste1)
-function paste1() {
-  navigator.clipboard.readText().then(
-    clipText => {
-      editor1.setValue(clipText)
-      editor1.getAction("editor.action.formatDocument").run()
-    }
-  )
-}
+function buildEditor(initialJSON) {
 
+  let startString = '{ "stuff": { "that": [1,2,3], "isin": true, "json": "end"}}'
+  if (initialJSON) {
+    startString = JSON.stringify(initialJSON)
+  }
 
-
-
-var editor2 = monaco.editor.create(document.getElementById('editor2'), {
-  value: [
-    '{ "stuff": { "that": [1,2,3], "isin": true, "json": "end"}}',
-  ].join('\n'),
-  language: 'json',
-  tabSize: 2,
-  formatOnPaste: true,
-  minimap: { enabled: false }
-});
-
-document.getElementById("format-edit-2").addEventListener("click", formatJson2)
-function formatJson2() {
-  editor2.getAction("editor.action.formatDocument").run()
-}
-
-document.getElementById("paste-2").addEventListener("click", paste2)
-function paste2() {
-  navigator.clipboard.readText().then(
-    clipText => {
-      editor2.setValue(clipText)
-      editor2.getAction("editor.action.formatDocument").run()
-    }
-  )
-}
-
-
-var originalModel = monaco.editor.createModel(editor1.getValue(), "text/json");
-var modifiedModel = monaco.editor.createModel(editor2.getValue(), "text/json");
-
-var diffEditor = monaco.editor.createDiffEditor(document.getElementById("diff-editor"));
-
-var navi = monaco.editor.createDiffNavigator(diffEditor, {
-  followsCaret: true, // resets the navigator state when the user selects something in the editor
-  ignoreCharChanges: true // jump from line to line
-});
-
-document.getElementById("refresh-diff").addEventListener("click", switchToDiff)
-
-function switchToDiff() {
-  originalModel = monaco.editor.createModel(editor1.getValue(), "text/json");
-  modifiedModel = monaco.editor.createModel(editor2.getValue(), "text/json");
-  diffEditor.setModel({
-    original: originalModel,
-    modified: modifiedModel
+  var editor1 = monaco.editor.create(document.getElementById('editor1'), {
+    value: [
+      startString,
+    ].join('\n'),
+    language: 'json',
+    tabSize: 2,
+    formatOnPaste: true,
+    minimap: { enabled: false }
   });
-}
 
-document.getElementById("next-change").addEventListener("click", nextDiff)
-function nextDiff() {
-  navi.next()
-}
+  document.getElementById("format-edit-1").addEventListener("click", formatJson1)
+  function formatJson1() {
+    editor1.getAction("editor.action.formatDocument").run()
+  }
 
-document.getElementById("previous-change").addEventListener("click", previousDiff)
-function previousDiff() {
-  navi.previous()
+  document.getElementById("paste-1").addEventListener("click", paste1)
+  function paste1() {
+    navigator.clipboard.readText().then(
+      clipText => {
+        editor1.setValue(clipText)
+        editor1.getAction("editor.action.formatDocument").run()
+      }
+    )
+  }
+
+
+
+
+  var editor2 = monaco.editor.create(document.getElementById('editor2'), {
+    value: [
+      '{ "stuff": { "that": [1,2,3], "isin": true, "json": "end"}}',
+    ].join('\n'),
+    language: 'json',
+    tabSize: 2,
+    formatOnPaste: true,
+    minimap: { enabled: false }
+  });
+
+  document.getElementById("format-edit-2").addEventListener("click", formatJson2)
+  function formatJson2() {
+    editor2.getAction("editor.action.formatDocument").run()
+  }
+
+  document.getElementById("paste-2").addEventListener("click", paste2)
+  function paste2() {
+    navigator.clipboard.readText().then(
+      clipText => {
+        editor2.setValue(clipText)
+        editor2.getAction("editor.action.formatDocument").run()
+      }
+    )
+  }
+
+
+  var originalModel = monaco.editor.createModel(editor1.getValue(), "text/json");
+  var modifiedModel = monaco.editor.createModel(editor2.getValue(), "text/json");
+
+  var diffEditor = monaco.editor.createDiffEditor(document.getElementById("diff-editor"));
+
+  var navi = monaco.editor.createDiffNavigator(diffEditor, {
+    followsCaret: true, // resets the navigator state when the user selects something in the editor
+    ignoreCharChanges: true // jump from line to line
+  });
+
+  document.getElementById("refresh-diff").addEventListener("click", switchToDiff)
+
+  function switchToDiff() {
+    originalModel = monaco.editor.createModel(editor1.getValue(), "text/json");
+    modifiedModel = monaco.editor.createModel(editor2.getValue(), "text/json");
+    diffEditor.setModel({
+      original: originalModel,
+      modified: modifiedModel
+    });
+  }
+
+  document.getElementById("next-change").addEventListener("click", nextDiff)
+  function nextDiff() {
+    navi.next()
+  }
+
+  document.getElementById("previous-change").addEventListener("click", previousDiff)
+  function previousDiff() {
+    navi.previous()
+  }
+
 }
